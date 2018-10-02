9c8a4ddc1691356ff9a6824982efce5aaea91e21
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2018-10-03 00:29:05 +0530

    Added Pagewise Search

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const xmlParser = require('xml2json');
 const cors = require('cors');
-const temp = require('./temp.json');
 const app = express();
 
 
@@ -15,14 +14,13 @@ app.get('/', (req, res) => {
 	res.json('database.users');
 });
 
-app.get('/search/:searchText', (request, response) => {
+app.get('/search/:searchText/:page', (request, response) => {
 	const {
-		searchText
+		searchText,
+		page
 	} = request.params;
 
-	// response.json(temp.GoodreadsResponse.search);
-
-	axios.get('https://www.goodreads.com/search.xml?key=a2v63G5HQqqbWau8bjzdFA&q='+ searchText )
+	axios.get('https://www.goodreads.com/search.xml?key=a2v63G5HQqqbWau8bjzdFA&q='+ searchText + '&page=' + page )
 	.then(xmlBooks => {
 		let jsonBooks = xmlParser.toJson( xmlBooks.data, {
 			object: true,
@@ -31,8 +29,6 @@ app.get('/search/:searchText', (request, response) => {
 		response.json(jsonBooks.GoodreadsResponse.search)
 	})
 
-
 });
-
 
 app.listen(3001);
