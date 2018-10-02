8795c44b805f1fab78f8160d76ca54b036fa29ec
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2018-10-03 00:53:44 +0530

    Moved to correct lines

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const xmlParser = require('xml2json');
 const cors = require('cors');
 const app = express();
 
-var port_number = server.listen(process.env.PORT || 3001);
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -30,5 +28,7 @@ app.get('/search/:searchText/:page', (request, response) => {
 	})
 
 });
+
+const port_number = process.env.PORT || 3001;
 
 app.listen(port_number);
